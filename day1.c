fedc6629240bd2efd3aeda24451c10a213af4d1d
vibhadubey210-ctrl <vibhadubey275@gmail.com>
2026-02-02 16:30:25 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,36 @@
+//Problem: Write a C program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.
+#include <stdio.h>
+
+int main() {
+    int n, pos, x;
+    int arr[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter position to insert (1-based): ");
+    scanf("%d", &pos);
+
+    printf("Enter element to insert: ");
+    scanf("%d", &x);
+
+    // Shift elements to the right
+    for(int i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // Insert the element
+    arr[pos - 1] = x;
+
+    printf("Updated array is:\n");
+    for(int i = 0; i <= n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
