94885b6755c30e47d0810337e2c6667f351de4e3
vibhadubey210-ctrl <vibhadubey275@gmail.com>
2026-04-17 23:34:26 +0530

    Add files via upload

---
+++ day77.c
@@ -0,0 +1,74 @@
+/*Problem Statement
+Using DFS or BFS, count number of connected components.
+
+Input Format
+n m
+edges
+
+Output Format
+Number of connected components.
+
+Sample Input
+6 3
+1 2
+2 3
+5 6
+
+Sample Output
+3
+
+Explanation
+Components: {1,2,3}, {4}, {5,6}*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int visited[MAX];
+int adj[MAX][MAX];
+
+// DFS function
+void dfs(int node, int n) {
+    visited[node] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        if (adj[node][i] == 1 && !visited[i]) {
+            dfs(i, n);
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    // initialize
+    for (int i = 1; i <= n; i++) {
+        visited[i] = 0;
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    // input edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1; // undirected graph
+    }
+
+    int count = 0;
+
+    // check all nodes
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            dfs(i, n);
+            count++;
+        }
+    }
+
+    printf("%d\n", count);
+
+    return 0;
+}
